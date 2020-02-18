13be5661b127b088f93d3fda13eb7fb740ff1eec
abmrifat1 <rifatabm1@gmail.com>
2020-02-18 20:11:55 +0600

    details page with pass id

--- rounting-demo/src/app/department-list/department-list.component.ts
+++ rounting-demo/src/app/department-list/department-list.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-department-list',
+  selector: "app-department-list",
   template: `
-    <p>
-      department-list works!
-    </p>
+    <h3>
+      Department List
+    </h3>
+    <ul class="items">
+      <li *ngFor="let department of departments">
+        <span class="badge">{{ department.id }}</span
+        >{{ department.name }}
+      </li>
+    </ul>
   `,
   styles: []
 })
 export class DepartmentListComponent implements OnInit {
+  departments = [
+    { id: 1, name: "Angular" },
+    { id: 2, name: "Node" },
+    { id: 3, name: "MongoDB" },
+    { id: 4, name: "React-native" },
+    { id: 5, name: "Laravel" }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
