6db65d63b22abcaf623d3309162f0b070ca26822
abmrifat1 <rifatabm1@gmail.com>
2020-02-19 15:09:12 +0600

    department by Id

--- rounting-demo/src/app/department-details/department-details.component.ts
+++ rounting-demo/src/app/department-details/department-details.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-department-details',
   template: `
     <p>
-      department-details works!
+      You selected {{departmentId}} no department.
     </p>
   `,
   styles: []
 })
 export class DepartmentDetailsComponent implements OnInit {
 
-  constructor() { }
+  public departmentId;
+  constructor( private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    let id = parseInt(this.route.snapshot.paramMap.get('id')); 
+    this.departmentId=id;
   }
 
 }
