102f98431da4689c0ad220868c8a1a55c1c01c16
abmrifat1 <rifatabm1@gmail.com>
2020-02-24 14:50:17 +0600

    submit disable when form is invalid

--- NgFormsTDF/src/app/app.component.ts
+++ NgFormsTDF/src/app/app.component.ts
@@ -9,6 +9,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'NgFormsTDF';
   topics = ['Angular','Node','React']
+topicHasError = true;
+validateTopic(value){
+  if(value=== 'default'){
+    this.topicHasError=true;
+  }
+  else{
+    this.topicHasError=false;
+  }
+}
 
   userModel= new User('','smith@test.com',90075556,'default','morning',true);
+
 }
