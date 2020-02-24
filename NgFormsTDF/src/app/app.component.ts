035fbfb6f44dd7f182846769f596a5b17e01f2c1
abmrifat1 <rifatabm1@gmail.com>
2020-02-24 15:38:15 +0600

    service added

--- NgFormsTDF/src/app/app.component.ts
+++ NgFormsTDF/src/app/app.component.ts
@@ -1,24 +1,33 @@
-import { User } from './user';
-import { Component } from '@angular/core';
+import { User } from "./user";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'NgFormsTDF';
-  topics = ['Angular','Node','React']
-topicHasError = true;
-validateTopic(value){
-  if(value=== 'default'){
-    this.topicHasError=true;
+  title = "NgFormsTDF";
+  topics = ["Angular", "Node", "React"];
+  topicHasError = true;
+  validateTopic(value) {
+    if (value === "default") {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
   }
-  else{
-    this.topicHasError=false;
+
+  onSubmit() {
+    console.log(this.userModel);
   }
-}
 
-  userModel= new User('','smith@test.com',90075556,'default','morning',true);
-
+  userModel = new User(
+    "",
+    "smith@test.com",
+    90075556,
+    "default",
+    "morning",
+    true
+  );
 }
