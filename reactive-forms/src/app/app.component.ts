9981c49b244aa601d2ba9fa317f4de43b9a72e06
abmrifat1 <rifatabm1@gmail.com>
2020-02-25 20:17:44 +0600

    error sms show for user name

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'reactive-forms';
-  registrationForm = new FormGroup({
-    userName: new FormControl('abm'),
-    password: new FormControl(''),
-    confirmPassword: new FormControl('')
+  constructor(private fb: FormBuilder) {}
+  title = "reactive-forms";
+  get userName(){
+    return this.registrationForm.get('userName');
+  }
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl("abm"),
+  //   password: new FormControl(""),
+  //   confirmPassword: new FormControl(""),
+  //   address: new FormGroup({
+  //     city: new FormControl(""),
+  //     state: new FormControl(""),
+  //     postalCode: new FormControl("")
+  //   })
+  // });
+
+  registrationForm = this.fb.group({
+    userName: ["",[ Validators.required,Validators.minLength(3)]],
+    password: [""],
+    confirmPassword: [""],
+    address: this.fb.group({
+      city: [""],
+      state: [""],
+      postalCode: [""]
+    })
   });
+
+  loadApiData() {
+    this.registrationForm.patchValue({
+      userName: "khorshed alam",
+      password: "123456",
+      confirmPassword: "123456"
+    });
+  }
 }
